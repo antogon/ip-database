7536835b53c61152a9ceca7e251d29ca78f0a45e
Antonio MalvaGomes <amalvag@clemson.edu>
2012-09-14 18:05:37 -0400

    Fixed all relations between IPAddress and Network.

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,3 +1,7 @@
 class Network < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :id,:name, :creator_id, :updater_id,
+		:router_name, :is_vrf, :is_hsrp, :desc, :vlan_no, :created_at, :updated_at
+	has_many :ip_addresses, :class_name => 'IPAddress', :foreign_key => :network_parent
+	has_one :network_no, :class_name => 'IPAddress', :primary_key => :network_no, :foreign_key => :id
+	has_one :netmask, :class_name => 'IPAddress', :primary_key => :netmask, :foreign_key => :id
 end
