5bd8ffeccbf45bc1f63d34b4297e3f8d4d896c86
Antonio MalvaGomes <amalvag@clemson.edu>
2012-09-14 17:27:42 -0400

    Related IPAddress to Network. Added IPv4 support to IPAddress.

--- app/models/ip_address.rb
+++ app/models/ip_address.rb
@@ -1,3 +1,12 @@
-class IpAddress < ActiveRecord::Base
-  # attr_accessible :title, :body
+class IPAddress < ActiveRecord::Base
+	belongs_to :network, :foreign_key => 'network_parent'
+
+	def ip_v4= new_ip
+		ip = IP.new(new_ip)
+		write_attribute(:ip_v4, ip.to_i)
+	end
+	
+	def ip_v4
+		IP.new(['v4',read_attribute(:ip_v4)]).to_s
+	end
 end
